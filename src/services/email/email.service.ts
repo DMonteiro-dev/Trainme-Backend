865828f2a25e7f4a8fa5dd47670d2c4795a8af6b
David <davidf.monteiro.4@gmail.com>
2026-01-07 11:48:43 +0000

    feat: configure mailtrap for password recovery

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT) || 2525,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
